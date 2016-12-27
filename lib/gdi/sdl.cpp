cb67b18bae4a0d4974bba21c7d248475e9e76852
Alex Maystrenko <alexeytech@gmail.com>
2016-12-27 13:51:14 +0100

    SDL allow window resize

--- lib/gdi/sdl.cpp
+++ lib/gdi/sdl.cpp
@@ -113,7 +113,7 @@ void gSDLDC::displayVideoFrame(GstBuffer *buf)
 
 void gSDLDC::evSetVideoMode(unsigned long xres, unsigned long yres)
 {
-	m_window = SDL_CreateWindow("enigma2-SDL2", 0, 0, xres, yres, 0);
+	m_window = SDL_CreateWindow("enigma2-SDL2", 0, 0, 1280, 720, SDL_WINDOW_RESIZABLE);
 	if (!m_window) {
 		eFatal("[gSDLDC] Could not create SDL window: %s", SDL_GetError());
 		return;
